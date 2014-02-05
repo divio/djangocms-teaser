5f770382bb2784b3e1e67a780bbbdd8f5dce70b7
Kim Thoenen <kim@smuzey.ch>
2014-02-05 18:29:02 +0100

    fixed imports for python3.3

--- djangocms_teaser/cms_plugins.py
+++ djangocms_teaser/cms_plugins.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
-from models import Teaser
+from .models import Teaser
 
 
 class TeaserPlugin(CMSPluginBase):
