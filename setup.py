0015d213e8598e06cbde56937ccab2b6d7fbf30b
Iacopo Spalletti <i.spalletti@nephila.it>
2014-09-04 08:02:08 +0200

    Fix setup.py to handle migrations_django package

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author='Divio AG',
     author_email='info@divio.ch',
     url='https://github.com/divio/djangocms-teaser',
-    packages=['djangocms_teaser', 'djangocms_teaser.migrations'],
+    packages=['djangocms_teaser', 'djangocms_teaser.migrations', 'djangocms_teaser.migrations_django'],
     license='LICENSE.txt',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
